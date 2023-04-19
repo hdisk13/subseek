5dd98f81c94b89ac93d5a289851318e949df3393
Scott Hunter <scott@tacomadata.com>
2023-04-18 19:59:21 -0700

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,21 @@
+module github.com/hdisk13/subseek
+
+go 1.20
+
+require (
+	github.com/Azure/azure-sdk-for-go v68.0.0+incompatible // indirect
+	github.com/Azure/go-autorest v14.2.0+incompatible // indirect
+	github.com/Azure/go-autorest/autorest v0.11.28 // indirect
+	github.com/Azure/go-autorest/autorest/adal v0.9.18 // indirect
+	github.com/Azure/go-autorest/autorest/azure/auth v0.5.12 // indirect
+	github.com/Azure/go-autorest/autorest/azure/cli v0.4.5 // indirect
+	github.com/Azure/go-autorest/autorest/date v0.3.0 // indirect
+	github.com/Azure/go-autorest/autorest/to v0.4.0 // indirect
+	github.com/Azure/go-autorest/autorest/validation v0.3.1 // indirect
+	github.com/Azure/go-autorest/logger v0.2.1 // indirect
+	github.com/Azure/go-autorest/tracing v0.6.0 // indirect
+	github.com/dimchansky/utfbom v1.1.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.2.0 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+)
---
+++ main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/Azure/azure-sdk-for-go/profiles/latest/resources/mgmt/subscriptions"
+	"github.com/Azure/go-autorest/autorest/azure/auth"
+)
+
+func readCredentialsFromFile() (string, string, string, error) {
+	file, err := os.Open("creds.config")
+	if err != nil {
+		return "", "", "", err
+	}
+	defer file.Close()
+
+	clientID, clientSecret, tenantID := "", "", ""
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "AZURE_CLIENT_ID=") {
+			clientID = strings.TrimPrefix(line, "AZURE_CLIENT_ID=")
+		} else if strings.HasPrefix(line, "AZURE_CLIENT_SECRET=") {
+			clientSecret = strings.TrimPrefix(line, "AZURE_CLIENT_SECRET=")
+		} else if strings.HasPrefix(line, "AZURE_TENANT_ID=") {
+			tenantID = strings.TrimPrefix(line, "AZURE_TENANT_ID=")
+		}
+	}
+
+	return clientID, clientSecret, tenantID, scanner.Err()
+}
+
+func main() {
+	ctx := context.Background()
+
+	// Configure the Azure SDK for Go with your Azure credentials
+	authorizer, err := auth.NewClientCredentialsConfig("<YOUR_AZURE_CLIENT_ID>", "<YOUR_AZURE_CLIENT_SECRET>", "<YOUR_AZURE_TENANT_ID>").Authorizer()
+	if err != nil {
+		fmt.Printf("Failed to get Azure authorizer: %v\n", err)
+		return
+	}
+
+	client := subscriptions.NewClient()
+	client.Authorizer = authorizer
+
+	// List Azure subscriptions
+	subs, err := client.List(ctx)
+	if err != nil {
+		fmt.Printf("Failed to list Azure subscriptions: %v\n", err)
+		return
+	}
+
+	fmt.Printf("%-40s %-36s %-36s %-10s\n", "Subscription_Name", "Subscription", "Tenant", "Is_Default")
+	for _, sub := range subs.Values() {
+		fmt.Printf("%-40s %-36s %-36s %-10v\n", *sub.DisplayName, *sub.SubscriptionID, *sub.TenantID, *sub.State == "Enabled")
+	}
+
+	fmt.Println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Activate which subscription?  (enter==don't change) >> ")
+	newSub, _ := reader.ReadString('\n')
+
+	if newSub == "" || newSub == "\n" {
+		fmt.Println("Keeping current subscription.")
+	} else {
+		_, err := client.Set(ctx, newSub[:len(newSub)-1])
+		if err != nil {
+			fmt.Printf("Failed to set Azure subscription: %v\n", err)
+			return
+		}
+		fmt.Println("Subscription changed.")
+	}
+}
