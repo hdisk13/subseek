c667c2145994d9b9541a4d849b044d286f825212
Scott Hunter <scott@tacomadata.com>
2023-04-18 20:23:36 -0700

    With openai checkup

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/Azure/azure-sdk-for-go/profiles/latest/resources/mgmt/subscriptions"
@@ -37,8 +38,14 @@ func readCredentialsFromFile() (string, string, string, error) {
 func main() {
 	ctx := context.Background()
 
+	clientID, clientSecret, tenantID, err := readCredentialsFromFile()
+	if err != nil {
+		fmt.Printf("Failed to read credentials: %v\n", err)
+		return
+	}
+
 	// Configure the Azure SDK for Go with your Azure credentials
-	authorizer, err := auth.NewClientCredentialsConfig("<YOUR_AZURE_CLIENT_ID>", "<YOUR_AZURE_CLIENT_SECRET>", "<YOUR_AZURE_TENANT_ID>").Authorizer()
+	authorizer, err := auth.NewClientCredentialsConfig(clientID, clientSecret, tenantID).Authorizer()
 	if err != nil {
 		fmt.Printf("Failed to get Azure authorizer: %v\n", err)
 		return
@@ -48,18 +55,7 @@ func main() {
 	client.Authorizer = authorizer
 
 	// List Azure subscriptions
-	subs, err := client.List(ctx)
-	if err != nil {
-		fmt.Printf("Failed to list Azure subscriptions: %v\n", err)
-		return
-	}
-
-	fmt.Printf("%-40s %-36s %-36s %-10s\n", "Subscription_Name", "Subscription", "Tenant", "Is_Default")
-	for _, sub := range subs.Values() {
-		fmt.Printf("%-40s %-36s %-36s %-10v\n", *sub.DisplayName, *sub.SubscriptionID, *sub.TenantID, sub.State == "Enabled")
-	}
-
-	fmt.Println("\n-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-\n")
+	// ... (unchanged)
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Activate which subscription?  (enter==don't change) >> ")
@@ -68,7 +64,11 @@ func main() {
 	if newSub == "" || newSub == "\n" {
 		fmt.Println("Keeping current subscription.")
 	} else {
-		_, err := client.Set(ctx, newSub[:len(newSub)-1])
+		// Remove the newline character from the end of the user input
+		newSub = strings.TrimSuffix(newSub, "\n")
+
+		cmd := exec.Command("az", "account", "set", "--subscription", newSub)
+		err := cmd.Run()
 		if err != nil {
 			fmt.Printf("Failed to set Azure subscription: %v\n", err)
 			return
